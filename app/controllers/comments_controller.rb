77cbc1811d66caecce331ca4d181b53d783709c0
Harry Turnbull <hturnbull@live.co.uk>
2020-05-09 18:59:14 +0100

    comment edit update routes

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,11 +1,19 @@
 class CommentsController < ApplicationController
 
+  def edit
+  end
+
   def create
     @post = Post.find(params[:post_id])
     @comment = @post.comments.create(comment_params)
     @comment.user_id = current_user.id
     @comment.save
+    p "created @comment id - #{@comment.id}"
     redirect_to post_path(@post)
+  end
+
+  def update
+    p params
   end
 
   private
